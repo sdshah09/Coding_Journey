dfc3f9bb893e362cac9447c76b3e5a43e8415f45
Shaswat Shah <shaswatshah51@gmail.com>
2024-04-10 22:25:45 -0500

    Added HashMap folder

---
+++ HashMap/Two_Sum.py
@@ -0,0 +1,42 @@
+'''
+
+Problem 1 (Easy)
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+'''
+class Solution:
+    def __init__(self) -> None:
+        pass
+    def twosum(self,nums,target):
+        target_dict = {}
+        for i in range(len(nums)):
+            val = abs(target-nums[i])
+            if val not in target_dict:
+                target_dict[nums[i]] = i
+            else:
+                return [target_dict[val],i]
+        return []
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twosum([3,3],6))
+
