1f8bb7466156625693d1316fa4723810d1c62e63
Shaswat Shah <shaswatshah51@gmail.com>
2024-08-08 16:00:16 -0500

    Added folder for stack

---
+++ Stack/Infix_To_Prefix.py
@@ -0,0 +1,58 @@
+'''
+Given an infix expression, the task is to convert it to a prefix expression.
+
+Infix Expression: The expression of type a ‘operator’ b (a+b, where + is an operator) i.e., when the operator is between two operands.
+
+Prefix Expression: The expression of type ‘operator’ a b (+ab where + is an operator) i.e., when the operator is placed before the operands.
+
+Examples: 
+
+Input: A * B + C / D
+Output: + * A B/ C D 
+
+Input: (A – B/C) * (A/K-L)
+Output: *-A/BC-/AKL
+
+Steps
+1. Reverse the string
+2.  Convert the reversed string to postfix from infix
+3. Again reverse the string that is your prefix answer
+'''
+
+class Solution:
+    
+    def priority(self,s):
+        if s == '^':
+            return 3
+        elif s in '*/':
+            return 2
+        elif s in '-+':
+            return 1
+        else:
+            return -1
+    #Function to convert an infix expression to a postfix expression.
+    def InfixtoPostfix(self, exp):
+        #code here
+        stack = []
+        s = ''
+        for i in exp:
+            if i.isalnum():
+                s+=i
+            elif i == '(':
+                stack.append(i)
+            elif i==')':
+                while(stack and stack[-1]!="("):
+                    s+=stack.pop()
+                stack.pop()
+            else:
+                while(stack and self.priority(i)<=self.priority(stack[-1])):
+                    s+=stack.pop()
+                stack.append(i)
+        while stack:
+            s += stack.pop()
+        return s
+
+    def InfixToPrefix(self,exp):
+        exp = exp[::-1]
+        prefix = self.InfixtoPostfix(exp)
+        return prefix[::-1]
